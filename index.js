8eba2e8f8c9da4734d4fefbb4fdd1d6969cbb20f
Priyam <119833929+priyam-12@users.noreply.github.com>
2023-01-03 07:18:11 +0000

    fix: Method 2 for EX: 4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //EX:01 helloWorld program
 console.log("Hello world!");
-
+//================================
 //EX:02 repeatString
 function repeatString(string,num){
   if (num < 0) return 'ERROR';
@@ -12,7 +12,7 @@ function repeatString(string,num){
 }
 
 console.log(repeatString("lo",4));
-
+//=================================
 //EX:03 reverseString
 function reverseString(string){
   //method 1
@@ -27,20 +27,50 @@ function reverseString(string){
 }
 
 console.log(reverseString("hello brother"));
-
+//===================================
 //EX:04 removeFromArray
-function removeFromArray(array,element){
-  //method 1
-  for(let i=0;i<array.length-1;i++){
-    if(array[i]===element){
-      let temp=array[i];
-      array[i]=array[i+1];
-      array[i+1]=temp;
+// function removeFromArray(array,element){
+//   //method 1
+//   for(let i=0;i<array.length-1;i++){
+//     if(array[i]===element){
+//       let temp=array[i];
+//       array[i]=array[i+1];
+//       array[i+1]=temp;
+//     }
+//   }
+//   array.pop();
+//   return array;
+// }
+
+//method 2
+// The easiest way to get an array of all of the arguments that are passed to a function
+// is using the rest operator. If this is unfamiliar to you look it up!
+const removeFromArray = function (...args) {
+  // the very first item in our list of arguments is the array, we pull it out with args[0]
+  const array = args[0];
+  // create a new empty array
+  const newArray = [];
+  // use forEach to go through the array
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
     }
-  }
-  array.pop();
-  return array;
-}
+  });
+  // and return that array
+  return newArray;
+};
+
+// A simpler, but more advanced way to do it is to use the 'filter' function,
+// which basically does what we did with the forEach above.
+
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
+//
 
 console.log(removeFromArray([1,2,3,4],3));
-
+//=====================================
